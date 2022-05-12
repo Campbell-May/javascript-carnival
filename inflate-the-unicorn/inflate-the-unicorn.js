e3712dd2760bcb92a3a0787a10428032d80906ad
campbell_may <campbellmay96.com>
2022-05-13 10:33:44 +1200

    Completed Inflate the unicorn

--- inflate-the-unicorn/inflate-the-unicorn.js
+++ inflate-the-unicorn/inflate-the-unicorn.js
@@ -1,5 +1,26 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Inflate The Unicorn!")
+console.log('Inflate The Unicorn!')
+
+let unicorn = document.getElementsByTagName('img')
+
+function clickUnicorn(e) {
+  let uni = e.target
+  console.log('hello')
+  if (uni.src.match('./images/unicorn-0.png')) {
+    uni.src = './images/unicorn-1.png'
+  } else if (uni.src.match('./images/unicorn-1.png')) {
+    uni.src = './images/unicorn-2.png'
+  } else if (uni.src.match('./images/unicorn-2.png')) {
+    uni.src = './images/unicorn-3.png'
+  } else if (uni.src.match('./images/unicorn-3.png')) {
+    alert('unicorn' + uni.id + ' Says thank you')
+  }
+}
+for (let i = 0; i < unicorn.length; i++) {
+  unicorn[i].onclick = clickUnicorn
+}
+
+let unicorn1 = {}
