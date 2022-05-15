e0e0992ff3444b98884e53737e91c730c557c805
campbell_may <campbellmay96.com>
2022-05-15 18:36:43 +1200

    made random Mole appear

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -1,5 +1,22 @@
-  // -    -   -   -   -  //
- // JAVASCRIPT CARNIVAL //
+// -    -   -   -   -  //
+// JAVASCRIPT CARNIVAL //
 // -    -   -   -   -  //
 
-console.log("Whack-a-Mole!")
+console.log('Whack-a-Mole')
+
+let moleTable = document.getElementsByTagName('td')
+let moleTableArr = Array.from(moleTable)
+
+console.log(moleTableArr)
+
+function getRandomItem(arr) {
+  let randomIndex = Math.floor(Math.random() * arr.length)
+  let randomCell = arr[randomIndex]
+
+  var img = document.createElement('img')
+  img.classList.add('mole')
+  img.src = './mole.PNG'
+
+  randomCell.appendChild(img)
+}
+getRandomItem(moleTableArr)
