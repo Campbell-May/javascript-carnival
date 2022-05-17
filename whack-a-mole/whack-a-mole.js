70888140218256eb2cf6e84441b6f1cdc94cf42a
campbell_may <campbellmay96.com>
2022-05-17 15:00:47 +1200

    Finished Whack a Mole

--- whack-a-mole/whack-a-mole.js
+++ whack-a-mole/whack-a-mole.js
@@ -13,10 +13,30 @@ function getRandomItem(arr) {
   let randomIndex = Math.floor(Math.random() * arr.length)
   let randomCell = arr[randomIndex]
 
-  var img = document.createElement('img')
+  let img = document.createElement('img')
   img.classList.add('mole')
   img.src = './mole.PNG'
 
   randomCell.appendChild(img)
 }
 getRandomItem(moleTableArr)
+
+function findMole() {
+  let hit = document.getElementsByClassName('mole')
+  for (let i = 0; i < hit.length; i++) {
+    hit[i].onclick = whackedMole
+  }
+}
+findMole()
+function whackedMole(e) {
+  let mole = e.target
+  console.log('hit')
+  if (mole.src.match('./mole.PNG')) {
+    mole.parentNode.removeChild(mole)
+    getRandomItem(moleTableArr)
+    var audio = new Audio()
+    audio.src = './whack-audio.wav'
+    audio.play()
+  }
+  findMole()
+}
